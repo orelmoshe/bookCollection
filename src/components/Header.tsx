228b3a8cd2f0767d878b5556c5e5cd66cc3a17f2
orelchenmoshe <orelchen.moshe@elbitsystems.com>
2025-01-20 11:33:55 +0200

    wip

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,15 +7,22 @@ import FilterField from './FilterField';
 import Logo from '../assets/images/Logo.png';
 import { BookContext, BookProps } from '../providers/BookProvider';
 
-const Container = styled('div')({
+const Container = styled('div')(({ theme }) => ({
+  position: 'fixed',
+  top: 0,
   height: 85,
   width: '100%',
   display: 'flex',
   justifyContent: 'space-around',
   alignItems: 'center',
   background: colors.blueDark,
+  zIndex: 3,
   boxShadow: `0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12)`,
-});
+  [theme.breakpoints.down('sm')]: {
+    flexDirection: 'column',
+    height: 150,
+  },
+}));
 
 const LogoWrapper = styled('div')({
   display: 'flex',
